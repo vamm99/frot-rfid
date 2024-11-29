c04d4ca09cb0e5b566cb5e25d9586183b83503ef
Víctor Alfonso Mejía Morales <130195315+vamm99@users.noreply.github.com>
2024-11-29 17:40:33 -0500

    Se agrego otra configuraciones finales

--- src/assets/echo.js
+++ src/assets/echo.js
@@ -5,10 +5,11 @@ import axios from "./apiCliente"; // Importamos nuestro cliente Axios
 window.Pusher = Pusher;
 // Configuración de Pusher y Echo
 const echo = new Echo({
-  broadcaster: "reverb",
-  key: "9ri8262cyqzlgdy3jny5", // Tu clave de Pusher (puedes obtenerla de tu cuenta de Pusher)
+  broadcaster: "pusher",
+  key: "9ri8262cyqzlgdy3jny5",
   wsHost: "apiapp.mooo.com",
-  wsPort: 8080,
+  wsPort: 8080, // Cambiar si el puerto 8080 no está permitido en producción
+  wssPort: 443, // Puerto estándar para WebSockets seguros
   forceTLS: true,
   encrypted: true,
   disableStats: true,
@@ -34,3 +35,39 @@ const echo = new Echo({
 
 // Exportamos la instancia de Echo para usarla en otros componentes
 export default echo;
+// import Echo from "laravel-echo";
+// import Pusher from "pusher-js";
+// import axios from "./apiCliente"; // Importamos nuestro cliente Axios
+
+// window.Pusher = Pusher;
+// // Configuración de Pusher y Echo
+// const echo = new Echo({
+//   broadcaster: "reverb",
+//   key: "9ri8262cyqzlgdy3jny5", // Tu clave de Pusher (puedes obtenerla de tu cuenta de Pusher)
+//   wsHost: "apiapp.mooo.com",
+//   wsPort: 8080,
+//   forceTLS: false,
+//   encrypted: true,
+//   disableStats: true,
+//   authorizer: (channel, options) => {
+//     return {
+//       authorize: (socketId, callback) => {
+//         // Autenticamos usando Axios para canales privados o presencia
+//         axios
+//           .post("/broadcasting/auth", {
+//             socket_id: socketId,
+//             channel_name: channel.name,
+//           })
+//           .then((response) => {
+//             callback(false, response.data); // Si la autenticación es exitosa
+//           })
+//           .catch((error) => {
+//             callback(true, error); // Si hay un error en la autenticación
+//           });
+//       },
+//     };
+//   },
+// });
+
+// // Exportamos la instancia de Echo para usarla en otros componentes
+// export default echo;
